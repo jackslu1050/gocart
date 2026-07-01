eb2e18d5e3cbd2aefa6d23e88d61287c9602c620
jackslu1050 <jackslumbu@hotmail.be>
2026-07-01 12:53:00 +0100

    Add project code files

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import {serve} from "inngest/next";
+
+import { inngest } from "@/inngest/client";
+import { syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/inngest/functions";
+
+export const {GET, POST, PUT}=serve({
+    client:inngest,
+    functions:[
+        syncUserCreation, syncUserUpdate, syncUserDeletion
+    ]
+})
